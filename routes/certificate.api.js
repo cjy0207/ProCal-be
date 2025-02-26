85676ebef01575299417330b66b38cad8063e5a4
cjy0207 <jiyunjiyun0723@naver.com>
2025-02-26 21:36:05 +0900

    se

--- routes/certificate.api.js
+++ routes/certificate.api.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const certificateController = require("../controllers/certificate.controller");
 
-// 자격증 CRUD API
-router.post("/", certificateController.createCertificate); // 자격증 추가
-router.get("/", certificateController.getAllCertificates); // 모든 자격증 조회
+router.post("/", certificateController.createCertificate); 
+router.get("/", certificateController.getAllCertificates); 
 router.get("/:id", certificateController.getCertificateById); // 특정 자격증 조회
 router.put("/:id", certificateController.updateCertificate); // 특정 자격증 수정
 router.delete("/:id", certificateController.deleteCertificate); // 특정 자격증 삭제
