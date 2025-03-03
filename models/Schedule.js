6636a7313569525664b255cb1daf1b424c5bb74f
cjy0207 <jiyunjiyun0723@naver.com>
2025-03-03 15:46:59 +0900

    ffinal

--- models/Schedule.js
+++ models/Schedule.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const ScheduleSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true },
   date: { type: Date, required: true },
   description: { type: String, default: "" }, 
